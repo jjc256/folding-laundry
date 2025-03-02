c6aedbb11877482451398ab2e64a2a0d5d59de65
Jiming Chen <jc3579@cornell.edu>
2025-03-01 21:28:45 -0500

    Fix imports again

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 export default {
   base: '/folding-laundry/',
+  build: {
+    rollupOptions: {
+      external: ['three']
+    }
+  },
   define: {
     'import.meta.env.PROD': JSON.stringify(process.env.NODE_ENV === 'production')
   }
